e717aad597d4d16fe580b815d1617e4b822418f6
Albelo27 <144069504+Albelo27@users.noreply.github.com>
2025-11-21 12:23:56 -0800

    Graph representation of PDA fully functional, no traversal engine.

--- PDAAnimator/PDAAnimator/Graph.cs
+++ PDAAnimator/PDAAnimator/Graph.cs
@@ -1,26 +1,23 @@
-﻿using System.Text.Json.Nodes;
+﻿using System.Text.Json;
+using System.Text.Json.Nodes;
 
 
 namespace PDAAnimator
 {   
     internal class Graph
     {
-        Dictionary<Node, Transition[]> graph = new Dictionary<Node, Transition[]>();
-        string fileLocation;
-        JsonArray states;
-        string initialState;
-        string[] acceptStates;
-        //I don't want to talk about this...
-        List<List<(string,string)>> transitionTable = new List<List<(string,string)>>();
+        public Dictionary<string, Node> graph = new Dictionary<string, Node>();
+        //construction variables
+        private string fileLocation;
+        private string initialState;
+        // JSON parse 
+        private JsonArray jsonStates;
+        private JsonArray jsonAcceptStates;
+        private JsonArray jsonTransTable;
 
         public Graph(string fileLocation) {
             this.fileLocation = fileLocation;
         }
-
-
-
-
-
 
         //create the PDA structure based on the formal description provided in the document
         public Boolean loadGraph() {
@@ -29,26 +26,136 @@ namespace PDAAnimator
             }
             return false;
         }
-
+        public int getStateCount()
+        {
+            return jsonStates.Count;
+        }
 
         private Boolean parseJson() {
             try
             {
+                //setup
                 StreamReader sr = new StreamReader(fileLocation);
                 string jsonPDA = sr.ReadToEnd();
-                System.Diagnostics.Debug.WriteLine(jsonPDA);
                 JsonNode node = JsonNode.Parse(jsonPDA)!;
-                states = node!["Q"]!.AsArray()!;
+                //begin graph object
+                jsonStates = node!["Q"]!.AsArray()!;
                 initialState = node["initial-state"]!.GetValue<string>();
+                jsonAcceptStates = node["accept-states"]!.AsArray()!;
+                jsonTransTable = node["transition-table"]!.AsArray();
+                //create nodes 
+                foreach (JsonNode q in jsonStates) {
+                    bool isAcceptState = jsonAcceptStates.Contains(q);
+                    List<Transition> transgender = new List<Transition>();
+                    Node newNode = new Node(q.ToString(), transgender, isAcceptState);
+                    graph.Add(newNode.getName(), newNode);
+                }
+                //make the transitions
+                foreach (JsonNode t in jsonTransTable) {
+                    //System.Diagnostics.Debug.WriteLine(t);
+                    var readStack = t["pop"]!.ToString();
+                    var writeStack = t["push"]!.ToString();
+                    var input = t["input"]!.ToString();
+                    Node toNode;
+                    Node fromNode;
+                    if (graph.ContainsKey(t["state"]!.ToString()))
+                    {
+                        graph.TryGetValue(t["state"]!.ToString(), out fromNode);
+                    } else {
+                        throw new JsonException("improper state format in transition table");
+                    }
+                    if (graph.ContainsKey(t["to-state"]!.ToString()))
+                    {
+                        graph.TryGetValue(t["to-state"]!.ToString(), out toNode);
+                    }
+                    else {
+                        throw new JsonException("improper to-state format in transition table");
+                    }
+                        Transition newTransition = new Transition(input, readStack, writeStack, fromNode!, toNode!);
+                    fromNode.addEdge(newTransition);
+                }
+                System.Diagnostics.Debug.WriteLine(graph.Count);
                 return true;
             }
             catch (Exception e) {
                 return false;
             }
         }
-
-        public int getStateCount() {
-            return states.Count;
+        
+        //example JSON:
+        /*
+         *{
+    "Q": [
+        "q0",
+        "q1",
+        "q2",
+        "q3"
+    ],
+    "sigma": [
+        "0",
+        "1"
+    ],
+    "gamma": [
+        "0",
+        "1",
+        "$"
+    ],
+    "transition-table": [
+        {
+            "state": "q0",
+            "input": "e",
+            "to-state": "q1",
+            "pop": "e",
+            "push": "$"
+        },
+        {
+            "input": "0",
+            "to-state": "q1",
+            "state": "q1",
+            "pop": "e",
+            "push": "0"
+        },
+        {
+            "input": "1",
+            "to-state": "q1",
+            "state": "q1",
+            "pop": "e",
+            "push": "1"
+        },
+        {
+            "input": "e",
+            "state": "q1",
+            "to-state": "q2",
+            "pop": "e",
+            "push": "e"
+        },
+        {
+            "input": "0",
+            "to-state": "q2",
+            "state": "q2",
+            "pop": "0",
+            "push": "e"
+        },
+        {
+            "input": "1",
+            "to-state": "q2",
+            "state": "q2",
+            "pop": "1",
+            "push": "e"
+        },
+        {
+            "input": "e",
+            "state": "q2",
+            "to-state": "q3",
+            "pop": "$",
+            "push": "e"
         }
+    ],
+    "initial-state": "q0",
+    "accept-states": [
+        "q3"
+    ]
+} 
+*/
     }
 }
--- PDAAnimator/PDAAnimator/Node.cs
+++ PDAAnimator/PDAAnimator/Node.cs
@@ -1,19 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Security.Policy;
 using System.Text;
+using System.Windows.Forms.VisualStyles;
 
 namespace PDAAnimator
 {
     internal class Node
     {
-        string name;
-        Transition[] leavingEdges;
-
-        public Node(string name, Transition[] edges)
+        private string name;
+        private List<Transition> leavingEdges;
+        private bool isAcceptState;
+        /// <summary>
+        /// A node in the graph representation of a Push-Down Automata
+        /// </summary>
+        /// <param name="name">The name of the node.</param>
+        /// <param name="edges">All leaving transitions.</param>
+        /// <param name="acceptState">True if node is an accepting state. Otherwise false.</param>
+        public Node(string name, List<Transition> edges, bool acceptState)
         {
             //Constructor go brrrrrrr
             this.name = name;
             this.leavingEdges = edges;
+            this.isAcceptState = acceptState;
         }
+
+        public string getName() {
+            return name;
+        }
+
+        public List<Transition> getLeavingEdges() { return leavingEdges; }
+
+        public void addEdge(Transition edge) {
+            leavingEdges.Add(edge);
+        }
+
+        public bool isAccept() {
+            return isAcceptState;
+        }
+
     }
 }
